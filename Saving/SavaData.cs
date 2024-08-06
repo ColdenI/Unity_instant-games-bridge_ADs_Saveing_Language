9f7c9ba22369656b4c2be744841a167c5faee931
Colden I <84040711+ColdenI@users.noreply.github.com>
2024-08-06 12:14:43 +0300

    Update SavaData.cs

--- Saving/SavaData.cs
+++ Saving/SavaData.cs
@@ -1,23 +1,10 @@
 using System;
-using UnityEngine;
 
 [Serializable]
 public class SavaData
 {
     // general
     public int Money = 0;
-    public float Rating = 1f;
-    public bool isUnfinishedDay = false;  
-    public int pb_carPower = 0;
-    public bool pb_showCameraSpeedArea = false;
-    public bool pb_showCameraSpeedArea_isOn = false;
-    public int pb_carTank = 0;
-
-    // day
-    public int MoneyDay = 0;
-    public float RatingDay = 0;
-    public Fine[] fines = new Fine[0];
-    public float car_fuel = 250f;
 
     // settings
     public TranslationData_.Language language = TranslationData_.Language.Ru;
