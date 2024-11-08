08e5a37ef1d378050c090036811c177cad590152
Colden I <84040711+ColdenI@users.noreply.github.com>
2024-11-08 23:06:06 +0300

    Update SavaData.cs

--- Saving/SavaData.cs
+++ Saving/SavaData.cs
@@ -8,9 +8,6 @@ namespace CGL
         // general
         public int Money = 0;
         public int Level = 0;
-        public bool isUseMusic = true;
-        public bool isUseAllSound = true;
-        public float sensa = 1f;
 
         // settings
         public TranslationData.Language language = TranslationData.Language.Ru;
